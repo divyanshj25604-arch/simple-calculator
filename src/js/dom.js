ac3fdec8540c203e339ead348ee0e73950644f6e
divyansh jha <divyansh.j25604@nst.rishihood.edu.in>
2026-02-13 15:09:51 +0530

    making dom.js

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,1 +1,21 @@
-l
+//what this file does:
+// 	•	listens
+// 	•	calls calculator functions
+// 	•	updates display
+
+// what it does not:
+// 	•	No calculations
+// 	•	No arithmetic
+// 	•	No business logic
+// 	•	No state definitions
+
+//it has 3 responsibilities:
+	// 1.	Get references to:
+        // •	display
+        // •	all buttons
+	// 2.	Add click listeners
+	// 3.	Decide which calculator function to call based on button type
+
+// vairiables :
+let buttons = document.querySelectorAll(".button");  //access all the buttons from html
+let 
